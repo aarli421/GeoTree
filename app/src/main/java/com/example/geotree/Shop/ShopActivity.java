4b799227754e9e0ec95ca1a1fc5d495f60593f12
5up3rN00b <aaron.linear@gmail.com>
2020-04-11 20:31:30 -0700

    Added Shop

---
+++ app/src/main/java/com/example/geotree/Shop/ShopActivity.java
@@ -0,0 +1,16 @@
+package com.example.geotree.Shop;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.geotree.R;
+
+public class ShopActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shop);
+    }
+}
